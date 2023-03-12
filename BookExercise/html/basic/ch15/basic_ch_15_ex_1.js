d46fae33b78a306cf958150e19e19fed24856bb7
Zurab Gezalyan <mcdoss@mail.ru>
2023-03-12 20:00:50 +0300

    Chapter 15 basic in progress

---
+++ BookExercise/html/basic/ch15/basic_ch_15_ex_1.js
@@ -0,0 +1,17 @@
+let arg = prompt("Введите число?");
+switch (arg) {
+  case '0':
+  case '1':
+    alert( 'Один или ноль' );
+    break;
+
+  case '2':
+    alert( 'Два' );
+    break;
+
+  case 3:
+    alert( 'Никогда не выполнится!' );
+    break;
+  default:
+    alert( 'Неизвестное значение' );
+}
