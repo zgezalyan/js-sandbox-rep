75c00dc944d5c9f3a37c90f850af83cf90c7773f
Zurab Gezalyan <mcdoss@mail.ru>
2023-03-16 17:01:28 +0300

    Chapter 7 object basic complete

---
+++ BookExercise/html/objectsBasic/ch7/obj_basic_ch_7_ex_1.js
@@ -0,0 +1,45 @@
+let id = Symbol("id");
+let user = {
+  name: "Вася",
+  age: 30,
+  [id]: 123
+};
+
+for (let key in user) alert(key); // name, age (свойства с ключом-символом нет среди перечисленных)
+
+// хотя прямой доступ по символу работает
+alert( "Напрямую: " + user[id] );
+
+id = Symbol("id");
+user = {
+  [id]: 123
+};
+
+let clone = Object.assign({}, user);
+
+alert( clone[id] ); // 123
+
+// читаем символ из глобального реестра и записываем его в переменную
+id = Symbol.for("id"); // если символа не существует, он будет создан
+
+// читаем его снова и записываем в другую переменную (возможно, из другого места кода)
+let idAgain = Symbol.for("id");
+
+// проверяем -- это один и тот же символ
+alert( id === idAgain ); // true
+
+// получаем символ по имени
+let sym = Symbol.for("name");
+let sym2 = Symbol.for("id");
+
+// получаем имя по символу
+alert( Symbol.keyFor(sym) ); // name
+alert( Symbol.keyFor(sym2) ); // id
+
+let globalSymbol = Symbol.for("name");
+let localSymbol = Symbol("name");
+
+alert( Symbol.keyFor(globalSymbol) ); // name, глобальный символ
+alert( Symbol.keyFor(localSymbol) ); // undefined для неглобального символа
+
+alert( localSymbol.description ); // name
